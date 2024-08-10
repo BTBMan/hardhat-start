434c4abe78c20ecc3d318ac3ad644b07d95ecc77
BTBMan <1498118157@qq.com>
2024-08-10 20:46:03 +0800

    feat: hardhat task
    Signed-off-by: BTBMan <1498118157@qq.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+task('accounts', 'Prints the list of accounts', async (_, hre) => {
+  const signers = await hre.ethers.getSigners();
+
+  for (const account of signers) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
 };
 
 export default config;
