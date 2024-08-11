478c9e4b96fd5489439fc87c7f44069f14ec0fc2
BTBMan <1498118157@qq.com>
2024-08-11 16:31:04 +0800

    feat: call and read event
    Signed-off-by: BTBMan <1498118157@qq.com>

--- contracts/FirstHardhatContract.sol
+++ contracts/FirstHardhatContract.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.24;
 contract FirstHardhatContract {
     constructor() {}
 
+    event MyEvent(address sender);
+
     string public name = "FirstHardhatContract";
 
     function getName() public view returns (string memory) {
         return name;
+    }
+
+    function triggerEvent() public {
+        emit MyEvent(msg.sender);
     }
 }
--- ignition/modules/FirstHardhatContract.ts
+++ ignition/modules/FirstHardhatContract.ts
@@ -5,6 +5,14 @@ const FirstHardhatContractModule = buildModule(
   (builder) => {
     const contract = builder.contract('FirstHardhatContract');
 
+    const contractName = builder.staticCall(contract, 'getName', []);
+    const triggerEvent = builder.call(contract, 'triggerEvent', []);
+
+    const value = builder.readEventArgument(triggerEvent, 'MyEvent', 0);
+
+    console.log(value);
+    console.log(contractName);
+
     return { contract };
   },
 );
