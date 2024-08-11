918276fb21d7d4d195e064b0d7865e580826b92a
BTBMan <1498118157@qq.com>
2024-08-11 17:40:40 +0800

    feat: gas report and solidity test coverage
    Signed-off-by: BTBMan <1498118157@qq.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,13 @@
 import { HardhatUserConfig, task, vars } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
 import 'dotenv/config';
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COIN_MARKET_KEY = process.env.COIN_MARKET_KEY;
 
 task('accounts', 'Prints the list of accounts', async (_, hre) => {
   const signers = await hre.ethers.getSigners();
@@ -14,7 +17,7 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
   }
 });
 
-task('test', 'Just test', async () => {
+task('print', 'Just test', async () => {
   console.log(SEPOLIA_RPC_URL, PRIVATE_KEY);
 });
 
@@ -34,6 +37,13 @@ const config: HardhatUserConfig = {
       url: 'http://127.0.0.1:8545/',
       chainId: 31337, // this is the default chainId of the hardhat network
     },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COIN_MARKET_KEY,
   },
 };
 
