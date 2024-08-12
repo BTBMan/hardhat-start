73890ddbdba99accf79933fad1a0383f58980309
BTBMan <1498118157@qq.com>
2024-08-12 23:58:06 +0800

    chore: skip unused tests
    Signed-off-by: BTBMan <1498118157@qq.com>

--- test/FirstHardhatContract.ts
+++ test/FirstHardhatContract.ts
@@ -2,7 +2,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { expect } from 'chai';
 import hre from 'hardhat';
 
-describe('FirstHardhatContract', () => {
+describe.skip('FirstHardhatContract', () => {
   const deployFixture = async () => {
     // const signers = await hre.ethers.getSigners();
 
