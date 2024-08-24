602e24182d9f107cf5972050e2e3a5b9705a34c3
BTBMan <1498118157@qq.com>
2024-08-24 23:53:07 +0800

    feat: withdraw and getBalance
    Signed-off-by: BTBMan <1498118157@qq.com>

--- html-website/main.js
+++ html-website/main.js
@@ -4,9 +4,23 @@ import { abi, address } from './constants.js';
 
 const connectBtn = document.getElementById('connect');
 const fundBtn = document.getElementById('fund');
+const withdrawBtn = document.getElementById('withdraw');
+const getBalanceBtn = document.getElementById('get-balance');
+const balanceValue = document.getElementById('balance-value');
 
 connectBtn.onclick = connect;
-fundBtn.onclick = fund.bind(this, '2');
+fundBtn.onclick = fund;
+withdrawBtn.onclick = withdraw;
+getBalanceBtn.onclick = getBalance;
+
+async function getBalance() {
+  if (window.ethereum) {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const balance = await provider.getBalance(address);
+
+    balanceValue.innerText = ethers.formatEther(balance) + ' HETH';
+  }
+}
 
 async function connect() {
   if (window.ethereum) {
@@ -20,12 +34,53 @@ async function connect() {
   }
 }
 
-async function fund(ethAmount) {
+async function fund() {
   if (window.ethereum) {
+    const ethAmount = document.getElementById('eth-amount').value;
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner(); // current connected wallet
     const contract = new ethers.Contract(address, abi, signer);
 
-    await contract.fund({ value: ethers.parseEther(ethAmount) });
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log('Done!!!');
+    } catch (error) {
+      console.log('reject!');
+    }
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  const transactionHash = transactionResponse.hash;
+  console.log(`mining ${transactionHash}...`);
+
+  return new Promise((resolve) => {
+    provider.once(transactionHash, async (transactionReceipt) => {
+      console.log(`transaction address: ${transactionReceipt.to}`);
+      console.log(
+        `completed with ${await transactionReceipt.confirmations()} confirmations`,
+      );
+      resolve();
+    });
+  });
+}
+
+async function withdraw() {
+  if (window.ethereum) {
+    const ethAmount = document.getElementById('eth-amount').value;
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner(); // current connected wallet
+    const contract = new ethers.Contract(address, abi, signer);
+
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log('Done!!!');
+    } catch (error) {
+      console.log('reject!');
+    }
   }
 }
